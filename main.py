fc8cd6d878b6cc26ed756db2f09c28367c32010e
Michael8431 <mgm8431@gmail.com>
2025-02-07 17:50:12 -0500

    creating of card objects and displaying in coordinate system.

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main():
                 running = False
 
         # fill the screen with a color to wipe away anything from last frame
-        screen.fill("purple")
+        screen.fill("blue")
 
         # RENDER YOUR GAME HERE
         #Create group of cards
@@ -34,30 +34,12 @@ def main():
         
         jokers = []
         for row_i in range(4):
+            row = []
             for col_i in range(6):
                 this_card = Card(screen, 'joker_card.jpg', 0, (row_i+1,col_i+1))
                 this_card.place_card()
-                jokers.append(this_card)
-        # test_card_img = pygame.image.load("joker_card.jpg").convert() #adds image from file
-        # screen.blit(test_card_img, (0,0),)
-        # joker1 = Card(screen, 'joker_card.jpg', 0, (1,1))
-        # joker1.place_card()
-        # joker2 = Card(screen, 'joker_card.jpg', 0, (1,2))
-        # joker2.place_card()
-        # joker3 = Card(screen, 'joker_card.jpg', 0, (1,3))
-        # joker3.place_card()
-        # joker4 = Card(screen, 'joker_card.jpg', 0, (1,4))
-        # joker4.place_card()
-        # joker5 = Card(screen, 'joker_card.jpg', 0, (1,5))
-        # joker5.place_card()
-        # joker6 = Card(screen, 'joker_card.jpg', 0, (2,1))
-        # joker6.place_card()
-        # joker7 = Card(screen, 'joker_card.jpg', 0, (3,1))
-        # joker7.place_card()
-        # joker8 = Card(screen, 'joker_card.jpg', 0, (4,1))
-        # joker8.place_card()
-        # joker3 = Card(screen, 'joker_card.jpg', 0, (1,2))
-        # joker3.place_card()
+                row.append(this_card)
+            jokers.append(row)
 
         # flip() the display to put your work on screen
         pygame.display.flip()
